f3f980ace9aff759126931febc913fee9c7db0aa
BrunaScripts <brunabarbosadealencar@gmail.com>
2024-02-22 17:15:52 -0300

    Agora temos template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def portao(request):
+    return HttpResponse("Você cegou ao portão da casa")
+
+def sala (request):
+    return HttpResponse("Você chegou na sala")
+
+def quarto (request):
+    return HttpResponse("Você chegou no quarto")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
