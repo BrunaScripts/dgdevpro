b5b0bdbc41e6b1bebe3ff30097a4250d0f610dd4
BrunaScripts <brunabarbosadealencar@gmail.com>
2024-02-22 14:52:46 -0300

    Meus arquivos do DG

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+#admin.site.register(Post) <-- pouca cutomização
+
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ("author", "title", "text", "created_date", "published_date")
